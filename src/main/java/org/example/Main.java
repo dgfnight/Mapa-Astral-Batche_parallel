8464dd9719b5c895c9787f1b3484a262006350ac
derik <degaloni@gmail.com>
2022-10-11 11:16:53 -0300

    Criando Aplicação

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,14 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
 
         String HOME_PATH = System.getProperty("user.dir");
         System.out.println(HOME_PATH);
@@ -30,14 +34,49 @@ public class Main {
         });
 
 
-        pessoaList.forEach((pessoa -> {
+        pessoaList.stream().parallel().forEach((pessoa -> {
             String caminho = HOME_PATH.concat("/files/" + pessoa.getNome() + ".txt");
             try {
                 Arquivo.criar(caminho, pessoa.gerarRelatorio());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+
         }));
 
     }
 }
+//}
+//
+//        criarArquivos(HOME_PATH, pessoaList);
+//
+//
+////        Executor executor = Executors.newFixedThreadPool(6);
+////
+////        final List<CompletableFuture> arquviosfuture = IntStream.rangeClosed(1,6)
+////                .boxed()
+////                .map(i -> CompletableFuture.supplyAsync(()-> criarArquivos(i.toString(), executor))
+////                .collect(Collectors.toList());
+//
+//
+//
+////
+////    }
+//
+//   }
+//
+//    private static void criarArquivos(String HOME_PATH, List<Pessoa> pessoaList) {
+//        pessoaList.stream().parallel().forEach(pessoa -> {
+//            String caminho = HOME_PATH.concat("/files/" + pessoa.getNome() + ".txt");
+//            try {
+//                Arquivo.criar(caminho, pessoa.gerarRelatorio());
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//
+//        }
+//
+//
+//    }
+//}
